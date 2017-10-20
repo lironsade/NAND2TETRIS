ab2e4dcbfe5ac6e216820067161ccf9d48453a46
Liron Sade <liron.sade@mail.huji.ac.il>
2017-10-20 13:03:02 +0300

    some

--- 07/VMTranslator.py
+++ 07/VMTranslator.py
@@ -5,56 +5,6 @@ import sys
 # translate memory access (push/pop)
 # translate arithmetic
 # ignore comments
-
-
-
-
-
-
-#### parsing helpers ####
-def isDoNothingLine(line):
-    if line.startswith("//"):
-        return True
-    if line.startswith("("):
-        return True
-    return not(line)
-
-def convertNumToBinary(num):
-    return '{0:015b}'.format(int(num))
-
-def parseDest(dest):
-    return destTable.get(dest)
-
-def parseJmp(jmp):
-    return jmpTable.get(jmp)
-        
-def parseComp(comp):
-    return compTable.get(comp)
-
-def breakDownCInstruction(line):
-    firstHalf = line.split('=')
-    dest = None if len(firstHalf) == 1 else firstHalf[0]
-    secondHalf = firstHalf[-1].split(';')
-    comp = secondHalf[0]
-    jmp = None if len(secondHalf) == 1 else secondHalf[1]
-    return (dest, comp, jmp)
-
-#### Translation ####
-def translateAInstruction(line):
-    global symbolsTable
-    global freeAddress
-    variable = line[1:]
-    if variable.isdigit():
-        return '0' + convertNumToBinary(variable)
-    if variable not in symbolsTable:
-        symbolsTable[variable] = freeAddress
-        freeAddress += 1
-    return '0' + convertNumToBinary(symbolsTable[variable])
-
-def translateCInstruction(line):
-    dest, comp, jmp = breakDownCInstruction(line)
-    return '111' + parseComp(comp) + parseDest(dest) + parseJmp(jmp)
-
 def translateSegment(segment):
     if (segment == 'local'):
         return 'LCL'
@@ -70,6 +20,48 @@ def parseMemoryAccess(line):
     # splitted 1 = segment, splitted 2 = i
     return splitted[1], splitted[2]
 
+def translatePushConstant(i):
+    return '@'+ i +'\n'
+           'D=A\n' + \
+           '@SP\n' + \
+           'A=M\n' + \
+           'M=D\n' + \
+           '@SP\n' + \
+           'M=M+1\n'
+
+def translatePushStatic(i):
+    return '@Foo.' + i + '\n' +\
+           'D=M\n' + \
+           '@SP\n' + \
+           'A=M\n' + \
+           'M=D'
+
+def translatePopStatic(i):
+    return '@SP\n' +\
+           'M=M-1\n' + \
+           'A=M\n' + \
+           'D=M\n' + \
+           '@Foo.' + i + '\n' + \
+           'M=D\n' + \
+           '@SP\n' + \
+           'M=M+1\n'
+
+def translatePushTemp(i):
+    return '@Foo.' + i + '\n' +\
+           'D=M\n' + \
+           '@SP\n' + \
+           'A=M\n' + \
+           'M=D'
+
+def translatePopTemp(i):
+    return '@SP\n' +\
+           'M=M-1\n' + \
+           'A=M\n' + \
+           'D=M\n' + \
+           '@Foo.' + i + '\n' + \
+           'M=D\n' + \
+           '@SP\n' + \
+           'M=M+1\n'
 
 def translatePush(line):
     # addr = segmentPointer + i
@@ -77,19 +69,12 @@ def translatePush(line):
     # SP++
     segment, i = parseMemoryAccess(line)
     if (segment == 'constant'):
-        return '//' + line +'\n' + \
-                '@'+ i +'\n'
-                'D=A\n' + \
-                '@SP\n' + \
-                'A=M\n' + \
-                'M=D\n' + \
-                '@SP\n' + \
-                'M=M+1\n'
+        return translatePushConstant(i)
+    if (segment == 'static'):
     
                 
     segment = translateSegment(segment)
-    return  '//'+ line + '\n' + \
-            '@'+segment + '\n'+ \
+    return  '@'+segment + '\n'+ \
             'D=M\n'+ \
             '@' + i + '\n'+ \
             'A=D+A\n' + \
@@ -105,8 +90,7 @@ def translatePop(line):
     # *addr = *SP
     segment, i = parseMemoryAccess(line)
     segment = translateSegment(segment)
-    return  '//'+ line + '\n' + \
-            '@'+segment + '\n'+ \
+    return  '@'+segment + '\n'+ \
             'D=M\n'+ \
             '@' + i + '\n'+ \
             'D=D+A\n' + \
@@ -177,6 +161,7 @@ def main(inputFileName):
     for line in inputFile:
         parsed = parseLine(line)
         if parsed:
+            outputFile.write("//" + line + "\n")
             outputFile.write(parsed + "\n")
 
     inputFile.close();
